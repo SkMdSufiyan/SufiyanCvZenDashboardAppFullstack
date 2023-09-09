860869670b4b89b82fa75124157198c749d4ae30
SkMdSufiyan <107339274+SkMdSufiyan@users.noreply.github.com>
2023-09-10 00:57:18 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,9 +27,9 @@ const Home = () => {
     </Navbar>
 
     
-    <p className='app-suggestion-p'>When you have opened this application's link for the <b style={{color : "blue"}}>first time</b>, please wait for 20-30 seconds <b style={{color : "blue"}}>after submitting the requests for login, signup, or forgot password</b>.</p>
+    <p className='app-suggestion-p'>When you have opened this application's link for the <b style={{color : "blue"}}>first time</b>, please wait for 20-50 seconds <b style={{color : "blue"}}>after submitting the requests for login, signup, or forgot password</b>.</p>
     <p className='app-suggestion-p'>Due to Render's <b style={{color : "blue"}}>"spinning down on idle"</b> issue, the server may take a little longer time to start (when it is opened after a long time). </p>
-      
+    <p className='app-suggestion-p'>If it takes much longer time to respond <b style={{color : "blue"}}>(for the first time only)</b>, then try with closing and opening the app link one or two times.</p>
     <br />
 
         <h5 style={{color: "blue"}}>Welcome to Zen application </h5>
