c36e85e7989553435ee756d749a22829c8b335fa
SkMdSufiyan <107339274+SkMdSufiyan@users.noreply.github.com>
2023-09-11 02:40:35 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
     </Navbar>
 
     
-    <p className='app-suggestion-p'><b style={{color : "blue"}}>The server of this application is deployed on a Render's free tier account</b>. Due to Render's <b style={{color : "blue"}}>"spinning down on idle"</b> issue, the server may take a little longer to start (when it is opened after a long time).</p>
+    <p className='app-suggestion-p'><b style={{color : "blue"}}>The server of this application is deployed on a Render's free tier account</b>. Due to Render's <b style={{color : "blue"}}>"spinning down on idle"</b> issue, the server may (sometimes) take a little longer to start (when it is opened after a long time).</p>
     <p className='app-suggestion-p'>When you open this application's link for the <b style={{color : "blue"}}>first time</b>, please wait 15-30 seconds <b style={{color : "blue"}}>after submitting the requests for login, signup, or forgot password</b>.</p>
     <p className='app-suggestion-p'>If it takes much longer to respond <b style={{color : "blue"}}>(for the first time only)</b>, try closing and opening the app link one or two times.</p>
     
